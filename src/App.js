0d04c6c9b450dd9b652cfd95478b16d041b355cb
rvangari96 <rvangari@itconvergence.com>
2022-12-15 17:37:03 +0530

    Unused code removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
 import Login from './components/login';
-import Logout from './components/logout';
 import { useEffect } from 'react';
 import { gapi } from 'gapi-script';
 const clientId = "479384257497-v6jverhaqhe76jbo0l6oiifspkavmfdk.apps.googleusercontent.com";
