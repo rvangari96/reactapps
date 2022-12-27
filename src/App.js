943a286f3ad4f09b9acc954f0370bf7d1455e5d4
rvangari96 <rvangari@itconvergence.com>
2022-12-27 17:50:34 +0530

    Label changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
         <div className='logo'>
           <img alt='logo' src="https://www.itconvergence.com/wp-content/uploads/2020/02/Logo_ITC_400x140.png" />
         </div>
-        <p>Hello Team.</p>
+        <p>Hello Team..</p>
         <p>Welcome to the demonstration of automating deployments using Jenkins.</p>
         <p>By Rakesh Vangari</p>
       </div>
